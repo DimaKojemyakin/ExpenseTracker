40623d716f686c24997888b88c98d25cf6cde34d
dima <dima@kojem.ru>
2024-01-10 17:30:13 +0300

    1

---
+++ ExpenseTracker/Views/RegistrView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrView.swift
+//  ExpenseTracker
+//
+//  Created by Дима Кожемякин on 10.01.2024.
+//
+
+import SwiftUI
+
+struct RegistrView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrView()
+}
